fa01f6f61d6bba2ddfa1ce146a8a3893fe9322ca
SowmithaBavirisetty <114602023+SowmithaBavirisetty@users.noreply.github.com>
2023-11-08 19:04:11 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,3 +1,27 @@
 def call(name) {
-  echo ("hi ${name}")
+ pipelineJob('DSL_Pipeline') {
+
+  //def repo = 'https://github.ibm.com/CryptoCAT/CLiC.git'
+
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm{
+      scm {
+        git {
+          remote { url($repo) }
+          branches('', '**/feature*')
+          scriptPath('hi.groovy')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+
+        // the single line below also works, but it
+        // only covers the 'master' branch and may not give you
+        // enough control.
+        // git(repo, 'master', { node -> node / 'extensions' << '' } )
+      }
+    }
+    sandbox()
+  }
+ }
 }
