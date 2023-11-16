d7101cee54ab93fd3c8a140603ceaeb39d5ea18c
SowmithaBavirisetty <114602023+SowmithaBavirisetty@users.noreply.github.com>
2023-11-16 16:01:13 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,22 +1,19 @@
 def call(var1) {
 pipeline {
-  environment {
-        
-      def hi = "${var1.vse}"  
-    }
+  
   agent any 
   stages {
     stage("script") {
       steps {
         script {
+          
         jobDsl scriptText: ''' 
-        environmentVariables {
-        keepSystemVariables(true)
-        }
-        def hlo = pipelineJob("${env.hi}")
-        hlo.with {
+        
+        
+        pipelineJob("exam") {
+        displayName(var1)
         def repo = 'https://github.com/SowmithaBavirisetty/sowmitha.git'
-
+        
         description("Pipeline for $repo")
         definition {
           cpsScm{
