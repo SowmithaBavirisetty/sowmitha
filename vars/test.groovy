9721dc93a542c20a6276d55763643c815c5cbde4
SowmithaBavirisetty <114602023+SowmithaBavirisetty@users.noreply.github.com>
2023-11-17 14:56:54 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -6,6 +6,7 @@ pipeline {
       steps {
         script {  
         writeFile file: 'display_name.txt', text: 'var = ${var1}'
+        echo (var)
         //shell('echo var1 > display_name.txt')
         jobDsl scriptText: ''' 
         String jobname = readFileFromWorkspace('display_name.txt').trim()
