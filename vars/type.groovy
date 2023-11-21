6494c7e40a05460aba55700afd68b46d7f7e2fa2
SowmithaBavirisetty <114602023+SowmithaBavirisetty@users.noreply.github.com>
2023-11-21 13:47:43 +0530

    Update type.groovy

--- vars/type.groovy
+++ vars/type.groovy
@@ -10,10 +10,8 @@ pipeline {
         //writeFile file: 'display_name.txt', text: var1        
         //shell('echo var1 > display_name.txt')
         jobDsl scriptText: ''' 
-         environmentVariables {
-          def jobname = "${var1}"
-         }
-         sh "BRANCH_NAME"
+         
+         def jobname = sh(script : ${BUILD_NUMBER}, returnStdout: true).trim()
         pipelineJob(jobname) {
         
         def repo = 'https://github.com/SowmithaBavirisetty/sowmitha.git'
