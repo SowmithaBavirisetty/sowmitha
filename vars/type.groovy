4f9f5c202d89fa22ba0e151a1e202369a42a3f45
SowmithaBavirisetty <114602023+SowmithaBavirisetty@users.noreply.github.com>
2023-12-01 12:36:01 +0530

    Update type.groovy

--- vars/type.groovy
+++ vars/type.groovy
@@ -10,12 +10,12 @@ pipeline {
         //writeFile file: 'display_name.txt', text: var1        
         //shell('echo var1 > display_name.txt')
         //def var = new "${var1}"
+        
+        
+        jobDsl scriptText: """  
         parameters {
           stringParam(name:'${var1}', defaultValue:"defaultval", description:'A parameter')
         }
-        
-        jobDsl scriptText: """  
-        
         pipelineJob(var1) {
         
         def repo = "https://github.com/SowmithaBavirisetty/sowmitha.git"
