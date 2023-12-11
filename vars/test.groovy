2e05240e1aaa828983bd96abb1dd0002d2416154
SowmithaBavirisetty <114602023+SowmithaBavirisetty@users.noreply.github.com>
2023-12-11 12:50:00 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,4 +1,4 @@
-def call(String config) {
+def call(String jobname) {
 pipeline {
   agent any 
   stages {
@@ -6,7 +6,7 @@ pipeline {
       steps {
         script {  
               
-        sh("echo ${config.var1} > display_name.txt")
+        sh("echo ${jobname} > display_name.txt")
         
         jobDsl scriptText: """
         
