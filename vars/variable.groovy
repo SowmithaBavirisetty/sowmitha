fb5052ec598ef1a6cf866987b8395d064b5900de
SowmithaBavirisetty <114602023+SowmithaBavirisetty@users.noreply.github.com>
2023-12-11 13:05:10 +0530

    Update variable.groovy

--- vars/variable.groovy
+++ vars/variable.groovy
@@ -1,2 +1,14 @@
-def mySharedVariable = 'sowmitha'
-return this
+// my-shared-library/vars/createPipelineJob.groovy
+def call(Map config) {
+    def jobName = config.jobName ?: 'defaultJobName'
+
+    pipelineJob(jobName) {
+        description("Pipeline for ${jobName}")
+        definition {
+            echo "Job name inside DSL script: ${jobName}"
+            
+            // Additional DSL script configurations...
+        }
+    }
+}
+
