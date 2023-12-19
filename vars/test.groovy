416c5b14d468e7d5857a71dc3c4581da99d2d283
SowmithaBavirisetty <114602023+SowmithaBavirisetty@users.noreply.github.com>
2023-12-19 17:50:09 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -11,7 +11,7 @@ pipeline {
         jobDsl scriptText: """
         
         String jobname = readFileFromWorkspace('display_name.txt').trim()
-        String Job  = 'Test-'+"${jobname}"
+        String Job  = "Test-"+"${jobname}"
         pipelineJob("'prerna'/'${Job}'") {
         
         def repo = "https://github.com/SowmithaBavirisetty/${jobname}.git"
