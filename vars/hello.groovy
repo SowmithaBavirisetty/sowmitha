fb2a6aa636a7cb0c4989b442b2a88aae16406c9c
SowmithaBavirisetty <114602023+SowmithaBavirisetty@users.noreply.github.com>
2024-01-04 14:33:27 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,7 +1,7 @@
 def call(List jobNames) {
-
+node {
     jobNames.each { jobName ->
-      jobDsl scriptText: """
+      
         pipelineJob("prerna/Test-${jobName}") {
             definition {
                 cpsScm {
@@ -14,7 +14,7 @@ def call(List jobNames) {
                     }
                 }
             }
-        } """
+        } 
     } 
-  //}    
+  }    
 }
