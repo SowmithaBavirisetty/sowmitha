b33555d0da7dad39d48375b244eab4d60181c816
SowmithaBavirisetty <114602023+SowmithaBavirisetty@users.noreply.github.com>
2024-01-11 11:11:12 +0530

    Update and rename script to example.groovy

---
+++ example.groovy
@@ -0,0 +1,46 @@
+def call(List jobname) {
+pipeline {
+  agent any 
+  stages {
+    stage("script") {
+      steps {
+        script {  
+         
+        //sh("echo ${jobname} >> display_name.txt")
+       // List jobnames = readFileFromWorkspace('display_name.txt').trim()
+
+        for (newvar in jobname) {
+        jobDsl scriptText: """
+    
+        
+        pipelineJob("prerna/Test-${newvar}") {
+        
+        def repo = "https://github.com/SowmithaBavirisetty/${newvar}.git"
+        
+        description("Pipeline for repo")
+        definition {
+          cpsScm{
+            scm {
+              git {
+                remote { url(repo)  }
+                credentialsId: 'key'
+                branches('testing')
+                scriptPath('hi.groovy')
+                extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+               
+              }
+            }
+          }
+        }
+      
+       }""" 
+      }
+        //sh "rm display_name.txt"
+        
+      }
+      }
+    }
+  }
+ }
+}
+  
