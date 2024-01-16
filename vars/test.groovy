1de829cd40fb4f0996becc79d5a8d3fb6ceed750
SowmithaBavirisetty <114602023+SowmithaBavirisetty@users.noreply.github.com>
2024-01-16 15:04:57 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -42,5 +42,13 @@ pipeline {
     }
   }
  }
+import org.jenkinsci.plugins.scriptsecurity.scripts.ScriptApproval
+
+ScriptApproval scriptApproval = ScriptApproval.get()
+scriptApproval.pendingScripts.each {
+    scriptApproval.approveScript(it.hash)
+}
+
+
 }
   
