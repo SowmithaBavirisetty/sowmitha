ab200db95a3c97fb2ca3c8cff8c95a27713f8edf
SowmithaBavirisetty <114602023+SowmithaBavirisetty@users.noreply.github.com>
2024-03-25 12:19:15 +0530

    Update hiii.groovy

--- vars/hiii.groovy
+++ vars/hiii.groovy
@@ -1,14 +1,14 @@
 def call(List var1) {
   script {
     for (newvar in var1) {
-      // The "jobDsl" is the pipeline step in jenkins used to write the DSL script
+      
       jobDsl scriptText: """
-        pipelineJob("prerna/Build_${newvar}") {     // "pipelineJob" is used to create the the job of type Pipeline in jenkins
+        pipelineJob("prerna/Build_${newvar}") {     
           def repo = "https://github.com/SowmithaBavirisetty/${newvar}.git"
           description("Pipeline for ${newvar}")
           definition {
-            cpsScm {                                     // Loads a pipeline script from SCM.    
-              scm {                                     // Specifies where to obtain a source code repository containing the pipeline script.
+            cpsScm {                                         
+              scm {                                     
                 git {
                   remote { 
                     url(repo)        
