89712d76f2e53069423f59c4aca4623da718dd5b
SowmithaBavirisetty <114602023+SowmithaBavirisetty@users.noreply.github.com>
2024-03-25 12:13:19 +0530

    Create hiii.groovy

---
+++ vars/hiii.groovy
@@ -0,0 +1,29 @@
+def call(List variantName) {
+  script {
+    for (newvar in variantName) {
+      // The "jobDsl" is the pipeline step in jenkins used to write the DSL script
+      jobDsl scriptText: """
+        pipelineJob("prerna/Build_${newvar}") {     // "pipelineJob" is used to create the the job of type Pipeline in jenkins
+          def repo = "https://github.com/SowmithaBavirisetty/${newvar}.git"
+          description("Pipeline for ${newvar}")
+          definition {
+            cpsScm {                                     // Loads a pipeline script from SCM.    
+              scm {                                     // Specifies where to obtain a source code repository containing the pipeline script.
+                git {
+                  remote { 
+                    url(repo) 
+                    credentials('CAT1_GHE_Full_PAT')       
+                  }
+                  branches("*/\\\${BRANCH}")
+                  scriptPath('Jenkinsfile')
+                }
+               }
+             }
+          }
+       }
+       queue("prerna/Build_${newvar}")
+       """
+      
+     }        
+   }
+}
